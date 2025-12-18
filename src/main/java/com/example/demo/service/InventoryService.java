e5685aca8c9a2fd873e0f06bd7b876bc647d1c01
bala-amypo <bala@amypo.in>
2025-12-18 18:47:07 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryService.java
+++ src/main/java/com/example/demo/service/InventoryService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+/* package com.example.demo.service;
 
 import com.example.demo.entity.InventoryLevel;
 import java.util.List;
@@ -15,3 +15,4 @@ public interface InventoryService {
 
     void delete(Long id);
 }
+ */

919dc301bde824b392ab3cc73e185ff3c07a83cd
bala-amypo <bala@amypo.in>
2025-12-18 09:03:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Store.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String storeName;
+    private String address;
+    private String region;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getStoreName() { return storeName; }
+    public void setStoreName(String storeName) { this.storeName = storeName; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getRegion() { return region; }
+    public void setRegion(String region) { this.region = region; }
+}

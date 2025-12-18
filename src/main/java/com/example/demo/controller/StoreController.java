f9b02a445623e5da27c7443225c9925ffa2b90de
bala-amypo <bala@amypo.in>
2025-12-18 14:21:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Store;
+import com.example.demo.service.StoreService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/stores")
+public class StoreController {
+
+    private final StoreService service;
+
+    public StoreController(StoreService service) {
+        this.service = service;
+        }
+
+        @PostMapping
+        public Store save(@Valid @RequestBody Store store) {
+            return service.save(store);
+        }
+
+        @GetMapping
+        public List<Store> getAll() {
+            return service.getAll();
+        }
+
+        @GetMapping("/{id}")
+        public Store get(@PathVariable Long id) {
+            return service.getById(id);
+            }
+
+            @DeleteMapping("/{id}")
+            public void delete(@PathVariable Long id) {
+                service.delete(id);
+            }
+}

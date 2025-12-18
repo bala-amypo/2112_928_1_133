e240b0686c14ef80c9d370cb2684f11dabaa612b
bala-amypo <bala@amypo.in>
2025-12-18 14:06:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.InventoryLevel;
+import com.example.demo.service.InventoryService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    private final InventoryService service;
+
+    public InventoryController(InventoryService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public InventoryLevel save(@Valid @RequestBody InventoryLevel inventory) {
+        return service.save(inventory);
+        }
+
+        @GetMapping
+        public List<InventoryLevel> getAll() {
+            return service.getAll();
+        }
+
+        @GetMapping("/{id}")
+        public InventoryLevel get(@PathVariable Long id) {
+            return service.getById(id);
+            }
+
+            @DeleteMapping("/{id}")
+            public void delete(@PathVariable Long id) {
+                service.delete(id);
+            }
+}

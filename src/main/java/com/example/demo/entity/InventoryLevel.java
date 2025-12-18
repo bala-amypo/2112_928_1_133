51087157e8b56f82db1b28088326dbd68cd624f3
bala-amypo <bala@amypo.in>
2025-12-18 13:17:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/InventoryLevel.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class InventoryLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Store store;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Store getStore() { return store; }
+    public void setStore(Store store) { this.store = store; }
+
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
+
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+}
--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -3,5 +3,4 @@ package com.example.demo.repository;
 import com.example.demo.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
+public interface StoreRepository extends JpaRepository<Store, Long> {}

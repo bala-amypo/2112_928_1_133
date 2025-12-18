065ee59ae48b72f883f73604321b9fc69465637d
bala-amypo <bala@amypo.in>
2025-12-18 13:21:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/InventoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.InventoryLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<InventoryLevel, Long> {}

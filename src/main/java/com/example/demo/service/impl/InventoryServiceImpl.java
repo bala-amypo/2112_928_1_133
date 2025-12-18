eb88982e5925d6aaa21fd0384a6f48f405407657
bala-amypo <bala@amypo.in>
2025-12-18 17:07:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.InventoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements InventoryService {
+    // No methods needed for now
+}

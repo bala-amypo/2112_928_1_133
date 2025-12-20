af523743165e3c4fa0ca9295fdb764ccc083d600
bala-amypo <bala@amypo.in>
2025-12-20 20:06:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/InventoryLevelController.java
+++ src/main/java/com/example/demo/controller/InventoryLevelController.java
@@ -1,4 +1,12 @@
-]@RestController
+package com.example.demo.controller;
+
+import com.example.demo.entity.InventoryLevel;
+import com.example.demo.service.InventoryLevelService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 @RequestMapping("/api/inventory")
 public class InventoryLevelController {
 

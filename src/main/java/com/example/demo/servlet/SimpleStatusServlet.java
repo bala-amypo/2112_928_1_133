3573ddc5f827dd22af586ab187f1b335158006e6
bala-amypo <bala@amypo.in>
2025-12-26 05:49:25 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -3,16 +3,17 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
 
-        resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("Multi-Location Inventory Balancer is running");
     }
 }
